7c6149cd884896945c8afcdffb6372f0fc8ef293
VitaliiMarlian <vmarlyan@mail.ru>
2023-09-27 22:28:24 +0300

    Fix rods view

--- FishingShop/store/views.py
+++ FishingShop/store/views.py
@@ -22,8 +22,16 @@ def rod_type(request):
 
 def rods(request, rod_type_id):
     current_rods = Rod.objects.filter(rod_type_id=rod_type_id)
-    manufacturers = Manufacturer.objects.all()
-    context = {'current_rods': current_rods, 'manufacturers': manufacturers}
+    # current_manufacturer_names = set()
+    # for current_rod in current_rods:
+    #   current_manufacturer_names.add(current_rod.rod_manufacturer_id.manufacturer_name)
+
+    # генерируем множество и делаем из него список
+    current_manufacturer_names = list(
+        {current_rod.rod_manufacturer_id.manufacturer_name for current_rod in current_rods})
+    # сортируем по алфавиту текущие названия производителей
+    current_manufacturer_names.sort()
+    context = {'current_rods': current_rods, 'current_manufacturer_names': current_manufacturer_names}
     return render(request, 'store/rods.html', context)
 
 
