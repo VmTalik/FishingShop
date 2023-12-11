270c0e78dd0e3f947e58566c186506be512fd5dc
VitaliiMarlian <vmarlyan@mail.ru>
2023-12-12 00:08:17 +0300

    Add and fix store app level routes

--- FishingShop/store/urls.py
+++ FishingShop/store/urls.py
@@ -1,11 +1,25 @@
-from django.urls import path
-from .views import index, catalog_summer, manufacturers, rod_type, rods, rod
+from django.urls import path, include
+from . import views
+from .views import index, product_categories, manufacturers, basket_detail, product_subcategories, ProductsView, \
+    ProductView
 
 urlpatterns = [
-    path('catalog-summer/rod_type/<int:rod_type_id>/<int:rod_id>/', rod, name='rod'),
-    path('catalog-summer/rod_type/<int:rod_type_id>/', rods, name='rods'),
-    path('catalog-summer/rod_type', rod_type, name='rod_type'),
-    path('catalog-summer', catalog_summer, name='catalog-summer'),
+    path('remove/<int:product_id>/', views.basket_remove, name='basket_remove'),
+    path('basket_update_quantity/<int:rod_id>/', views.basket_update_quantity, name='basket_update_quantity'),
+    path('basket_add/<int:rod_id>/', views.basket_add, name='basket_add'),
+    # path('<slug:fishing_season_slug>/<slug:category_slug>/<slug:subcategory_slug>/<int:rod_id>/', product, name='rod'),
+    # path('<slug:fishing_season_slug>/<slug:category_slug>/<slug:subcategory_slug>/<slug:product_slug>/',
+    #    product, name='product'),
+    path('<slug:fishing_season_slug>/<slug:category_slug>/<slug:subcategory_slug>/<slug:product_slug>/',
+         ProductView.as_view(), name='product'),
+    # path('<slug:fishing_season_slug>/<slug:category_slug>/<slug:subcategory_slug>/', products, name='products'),
+    path('<slug:fishing_season_slug>/<slug:category_slug>/<slug:subcategory_slug>/', ProductsView.as_view(),
+         name='products'),
+    path('<slug:fishing_season_slug>/<slug:category_slug>/', product_subcategories, name='product_subcategories'),
+    path('<slug:fishing_season_slug>/', product_categories, name='product_categories'),
+
+    path('basket', basket_detail, name='basket_detail'),
     path('brands', manufacturers, name='brands'),
     path('', index, name='index')
 ]
+
