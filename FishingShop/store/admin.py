16f29cfb791808b641316805b137c81f6d32f3c4
VitaliiMarlian <vmarlyan@mail.ru>
2024-02-24 22:03:30 +0300

    Add administration tools. Register models

--- FishingShop/store/admin.py
+++ FishingShop/store/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import (Product, AdditionalProductImage, Warehouse, Manufacturer, Category, SubCategory, FishingSeason,
+                     Step, Comment, ProductParameter, ProductParameterValue, ProductParameterValueStr, Customer, Buy,
+                     BuyStep)
 
-# Register your models here.
+
+class ProductParameterValueInline(admin.StackedInline):
+    model = ProductParameterValue
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductParameterValueInline]
+    search_fields = ('name', 'subcategory__subcategory_name', 'subcategory__category__category_name')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class BuyStepAdminInline(admin.StackedInline):
+    model = BuyStep
+
+
+class BuyAdmin(admin.ModelAdmin):
+    inlines = [BuyStepAdminInline]
+    search_fields = ('customer__username',)
+    ordering = ['-buystep__step_begin_datetime']
+
+
+admin.site.register([AdditionalProductImage,
+                     Warehouse,
+                     Manufacturer,
+                     Category,
+                     SubCategory,
+                     FishingSeason,
+                     Step,
+                     Comment,
+                     ProductParameter,
+                     ProductParameterValueStr,
+                     Customer])
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Buy, BuyAdmin)
